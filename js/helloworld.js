b478d278b7296f20712f1c12d3d0babe5d65db44
drikalabs <andrika.bogal@gmail.com>
2019-10-16 19:42:33 +0700

    Initial Commit js

---
+++ js/helloworld.js
@@ -0,0 +1,30 @@
+console.log("helloworld")
+var namaFungsi= new console.log('hello world')
+
+let num=2;
+function  withLet(){
+num2 =num*2;
+return num;
+}
+var anotherNum=3
+function anotherFungtion(){
+    anotherNum =anotherNum*5;
+    return (withLet()+anotherNum);
+}
+console.log("dengan hasilnya :"+anotherFungtion());
+
+var newnum=2;
+functionbaru();
+function functionbaru(){
+    hasil=num+2;
+    console.log(hasil);
+    perkalian(hasil);
+}
+perkalian(anotherFungtion())
+function perkalian(datanya){
+    let result=datanya*2;
+    console.log(result);
+}
+var tulisan ='ini tulisan';
+console.log(typeof newnum);
+console.log(typeof tulisan);
---
+++ js/operator.js
@@ -0,0 +1,24 @@
+//unary ==> kalo cuma operator operannya satu
+let numb = 1;
+numb = ++numb;
+console.log(numb)
+
+//binary
+let numb1 = 2,
+    numb2 = 1;
+console.log(numb1 - numb2);
+
+for (let index = 0; index < 5; index++) {
+    console.log(index);
+
+}
+
+let aple = "2";
+let orange = "1";
+
+console.log("3" * "3");
+console.log(+aple + +orange);
+console.log(aple - orange);
+console.log(13 & 5);
+console.log('12' == '12');
+console.log(12 === 12);
