c12a810256b14368a5b690f4ee0f34229dd52024
drikalabs <andrika.bogal@gmail.com>
2019-10-30 12:06:02 +0700

    create redux reducer

---
+++ react-redux-excercise19/src/counter/CounterContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CounterContainer extends React.Component {
+    render() {
+        return (
+            <div>
+
+                <button>+</button>
+                <h1>42</h1>
+                <button>-</button>
+            </div>
+
+        );
+    }
+}
+
+export default CounterContainer;
